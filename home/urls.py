7c027b7e401883ad083d53d2052ea36c6a843c16
Leonard Colin <leooo.colin@gmail.com>
2021-01-13 17:12:55 +0100

    Add footer and update nav-bar and login page

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.home_view, name="home"),
+    path('legal/', views.legal_view, name="legal")
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def home(request):
+def home_view(request):
     return render(request, 'home.html')
+
+
+def legal_view(request):
+    return render(request, 'legal.html')
