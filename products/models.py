ffbb86c97dc1030473471c8982aea3ca768f6a4a
Leonard Colin <leooo.colin@gmail.com>
2021-02-02 10:42:24 +0100

    Add Category and Product models

--- products/models.py
+++ products/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Category:
-    pass
+
+class Product(models.Model):
+    barcode = models.BigIntegerField()
+    name = models.CharField(max_length=250)
+    nutriscore = models.CharField(max_length=1)
+    image = models.URLField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    products = models.ManyToManyField(Product, db_table='assoc_cat_ali', related_name='categories')
+
+    def __str__(self):
+        return self.name
